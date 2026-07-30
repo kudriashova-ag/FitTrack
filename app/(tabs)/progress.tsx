a2f24243ad0295607920854008e7ab3a7f1f0a57
Anastacia <kudriashova.ag@gmail.com>
2026-07-30 18:20:21 +0300

    Pedometr

--- app/(tabs)/progress.tsx
+++ app/(tabs)/progress.tsx
@@ -1,42 +1,22 @@
-import { useState, useEffect } from "react";
+
+import usePedometer from "@/src/hooks/usePedometer";
 import { StyleSheet, Text, View } from "react-native";
-import { Pedometer } from "expo-sensors";
 
 export default function ProgressScreen() {
-  const [isPedometerAvailable, setIsPedometerAvailable] = useState("checking");
-  const [pastStepCount, setPastStepCount] = useState(0);
-  const [currentStepCount, setCurrentStepCount] = useState(0);
-
-  const subscribe = async () => {
-    const isAvailable = await Pedometer.isAvailableAsync();
-    setIsPedometerAvailable(String(isAvailable));
-
-    if (isAvailable) {
-      const end = new Date();
-      const start = new Date();
-      start.setDate(end.getDate() - 1);
-
-      const pastStepCountResult = await Pedometer.getStepCountAsync(start, end);
-      if (pastStepCountResult) {
-        setPastStepCount(pastStepCountResult.steps);
-      }
-
-      return Pedometer.watchStepCount((result) => {
-        setCurrentStepCount(result.steps);
-      });
-    }
-  };
-
-  useEffect(() => {
-    const subscription = subscribe();
-    return () => subscription && subscription.remove();
-  }, []);
-
+    const { isAvailable, isPermissionGranted, steps, error } = usePedometer();
+  
   return (
     <View style={styles.container}>
-      <Text>Pedometer.isAvailableAsync(): {isPedometerAvailable}</Text>
-      <Text>Steps taken in the last 24 hours: {pastStepCount}</Text>
-      <Text>Walk! And watch this go up: {currentStepCount}</Text>
+      {!isAvailable && (
+        <Text >
+          {error ?? "Перевірка доступності крокоміра..."}
+        </Text>
+      )}
+      {isAvailable && !isPermissionGranted && (
+        <Text>{error}</Text>
+      )}
+
+      <Text>{steps} кроків</Text>
     </View>
   );
 }
