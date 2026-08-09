808cf493fa96fe2bf9616c1dd7ce4ffdd26091e7
Anastacia <kudriashova.ag@gmail.com>
2026-08-09 18:10:33 +0300

    LocalNotifications

--- app.config.ts
+++ app.config.ts
@@ -64,6 +64,13 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
       },
     ],
     "expo-sqlite",
+    [
+      "expo-notifications",
+      {
+        icon: "./assets/images/icon.png",
+        color: "#ff3700",
+      },
+    ]
   ],
   extra: {
     router: {},
--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,78 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {  useEffect, useState } from 'react';
+import { Alert, Button, StyleSheet, Text, View } from 'react-native';
+import * as Notifications from 'expo-notifications';
+import { SchedulableTriggerInputTypes } from 'expo-notifications';
+
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+    shouldShowBanner: true,
+    shouldShowList: true,
+  }),
+});
+
+
 
 const ProfileScreen = () => {
+    const [done, setDone] = useState(false); // стан для відстеження, чи було виконано тренування
+
+    useEffect(() => { 
+        const permissionNotification = async () => { 
+            const { granted } = await Notifications.requestPermissionsAsync();
+            if (!granted) {
+                Alert.alert('Немає дозвлу на надсилання сповіщень');
+                return;
+            }
+        }
+        permissionNotification();
+    }, [])
+
+    // слухач для отримання сповіщень
+    useEffect(() => { 
+        const subscription = Notifications.addNotificationResponseReceivedListener((res) => { 
+            const data = res.notification.request.content.data;
+            Alert.alert('Отримано сповіщення', JSON.stringify(data))
+        });
+        return () => subscription.remove();
+    }, [])
+
+    const schedule = async () => { 
+        if (done) {
+            Alert.alert('Ви вже виконали тренування сьогодні');
+            return;
+        }
+
+        await Notifications.scheduleNotificationAsync({
+          identifier: "workout",
+          content: {
+            title: "Тренування",
+            body: "Час для тренування!",
+            data: { workoutId: 123 },
+          },
+          trigger: {
+            type: SchedulableTriggerInputTypes.TIME_INTERVAL,
+            seconds: 20, // через 20 секунд
+            channelId: "workout-channel",
+          },
+        });
+
+        Alert.alert('Сповіщення заплановано через 20 секунд');
+    }
+
+    const markDone = async () => {
+        setDone(true);
+        Alert.alert('Ви виконали тренування сьогодні, сповіщення більше не буде надсилатися');
+        await Notifications.cancelScheduledNotificationAsync("workout");
+    }
+
+
     return (
         <View>
-            <Text>Profile</Text>
+            <Text>{done ? 'Зроблено' : 'Не зроблено'}</Text>
+            <Button title="Запланувати сповіщення" onPress={schedule} />
+            <Button title="Позначити як зроблено" onPress={markDone} />
         </View>
     );
 }
